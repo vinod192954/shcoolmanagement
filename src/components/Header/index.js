c59e134fa59ee3045209517dec3cc26e78ac7cd0
vinod192954 <vvinnu590@gmail.com>
2024-09-03 21:31:48 +0530

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import "./index.css"
+const Header=()=>{
+    return (
+        <div className="header-container">
+            <div>
+                <h1>logo</h1>
+            </div>
+            <ul className="header-links">
+                <li>Home</li>
+                <li>Teacher</li>
+                <li>Student</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
